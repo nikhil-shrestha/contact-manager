21265a1abd7539b62e9ae7d26d4f15474522c6d4
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-14 18:59:48 +0545

    componets destructuring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Contact from './components/Contact';
+import Header from './components/Header';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>The App Component</h1>
-      <Contact />
+      <Header branding="Contact Manager" />
+      <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+      <Contact name="Karen Smith" email="karen@gmail.com" phone="333-333-333" />
     </div>
   );
 }
