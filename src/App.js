e57b92f16c274c2fef2100b6907a86426119765e
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-15 07:36:52 +0545

    creating form with state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
+import Contacts from './components/contacts/Contacts';
+import AddContact from './components/contacts/AddContact';
+import Header from './components/layout/Header';
 
 import { Provider } from './context';
 
@@ -12,6 +13,7 @@ function App() {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                name="name"
+                placeholder="Enter Name..."
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control form-control-lg"
+                name="email"
+                placeholder="Enter Email..."
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                name="phone"
+                placeholder="Enter Phone Number..."
+                value={phone}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
