60ad4bedfcdcc2412debc94e811a404aff1ab7e2
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-15 10:41:57 +0545

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <Provider>
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className="container">
-          <AddContact />
-          <Contacts />
-        </div>
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/add-contact" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
