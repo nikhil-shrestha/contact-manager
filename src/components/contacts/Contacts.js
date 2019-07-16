5fc499ab462f4e53d8aeaaf4e86db915c304a769
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-16 12:04:23 +0545

    creating seperate action file

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { GET_CONTACTS } from '../../actions/types';
+import { getContacts } from './../../actions/contactAction';
 
 class Contacts extends Component {
   componentDidMount() {
@@ -32,11 +32,7 @@ const mapStateToProps = state => ({
   contacts: state.contact.contacts
 });
 
-const mapDispatchToProps = dispatch => ({
-  getContacts: () => dispatch({ type: GET_CONTACTS })
-});
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  { getContacts }
 )(Contacts);
