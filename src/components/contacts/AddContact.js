b876ea622208a53f31e51311f7856840caae91a1
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2019-07-16 07:24:53 +0545

    using asyn-await

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -11,7 +11,7 @@ class AddContact extends Component {
     errors: {}
   };
 
-  onSubmit = (dispatch, e) => {
+  onSubmit = async (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
@@ -39,10 +39,11 @@ class AddContact extends Component {
       errors: {}
     };
 
-    axios
-      .post('http://jsonplaceholder.typicode.com/users', newContact)
-      .then(res => dispatch({ type: 'ADD_CONTACT', payload: res.data }));
-
+    const res = await axios.post(
+      'http://jsonplaceholder.typicode.com/users',
+      newContact
+    );
+    dispatch({ type: 'ADD_CONTACT', payload: res.data });
     this.setState({ name: '', email: '', phone: '', errors: {} });
 
     this.props.history.push('/');
